fdb65859786efd6c99de541f5674811274b099b5
Reynan Perez Albaredo <chezca1228@yahoo.com>
2013-06-13 15:29:27 +0800

    added files for reports and favicon

--- app/models/assigned_lawyer.rb
+++ app/models/assigned_lawyer.rb
@@ -1,5 +1,5 @@
 class AssignedLawyer < ActiveRecord::Base
   attr_accessible :file_matter_id, :client_id, :lawyer_id
-  has_many :lawyers
+  belongs_to :lawyer
   belongs_to :file_matters
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
   attr_accessible :name, :email, :contact_number, :position, :address, :contact_person
   belongs_to :file_matter
-  # belongs_to :case_entry
+  belongs_to :case_entry
 end
