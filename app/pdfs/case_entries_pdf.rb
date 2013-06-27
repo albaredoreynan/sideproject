64d834f2e5a0b9ab070f6d496f3a33c717480946
Reynan Perez Albaredo <chezca1228@yahoo.com>
2013-06-27 14:27:45 +0800

    Camelize data

--- app/pdfs/case_entries_pdf.rb
+++ app/pdfs/case_entries_pdf.rb
@@ -16,13 +16,13 @@ class CaseEntriesPdf < Prawn::Document
 		
 		row1 = []
 		row1 << [
-				{:content => 'Date', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 60 }, 
+				{:content => 'Entry Date', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 60 }, 
 				{:content => 'Start Time', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 60 },
 				{:content => 'End Time', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 60 },
 				{:content => 'Work Particulars', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold }, 
 				{:content => 'Client', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 85 },
 				{:content => 'Case Title', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold },
-				{:content => 'File Reference No', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 80 }
+				{:content => 'File Ref No.', :background_color => "E8E8D0", :align => :center, :text_color => "001B76", :font_style => :bold, :width => 80 }
 				]
 		
 		@case_entries.each do |case_entries|
@@ -38,13 +38,13 @@ class CaseEntriesPdf < Prawn::Document
 				case_entries.work_particulars,
 				@client_name,
 				case_entries.case_title,
-				case_entries.file_matter_case
+				{:content => "#{case_entries.file_matter_id}", :align => :center }
 			]		
 		end 
 			#row1 << [{:content => 'Employee Name', :colspan => 2 }, {:content => 'Client', :colspan => 2 }, {:content => 'Brand', :colspan => 2 }, {:content => 'Branch', :colspan => 2 }, {:content => 'Date Employed', :colspan => 2 } ]
 
 
-		table(row1, :width => 690, :cell_style => {:size => 9}) do
+		table(row1, :width => 720, :cell_style => {:size => 9}) do
 			self.row_colors = ["FFFFFF", "FFFFFF"]
 			self.header = false
 		end
