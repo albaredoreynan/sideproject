12ae6222df0da67739de39e9946d40aacc1494e3
Nad Flores <nflores@appsource.biz>
2013-12-28 02:43:31 +0800

    add highlight color link on subnav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
     		when :alert then "error"
     	end
     end
+
+  # method trigger for adding 'active' class on subnav.
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
+
 end
